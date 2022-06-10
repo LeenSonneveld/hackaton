624a2d4de135c9282563b0fc2b5c6ac5cfd8aedf
LeenSonneveld <leen.sonneveld@opensol.nl>
2022-06-10 14:26:12 +0200

    extra cleaning

--- 00_functions.R
+++ 00_functions.R
@@ -8,20 +8,32 @@ cleanup_brfss <- function(data){
                     "how_many_times_per_month_walking_running_etc",
                     "hours_walking_running_jogging_or_swimming",
                     "how_many_times_did_you_eat_dark_green_vegetables_per_week",
-                    "days_in_past_30_had_alcoholic_beverage")
+                    "days_in_past_30_had_alcoholic_beverage",
+                    "age_when_told_had_diabetes",
+                    "number_of_children_in_household")
+
+
+  # deze waarde worden vervangen door NA
+  na_values <- c("Refused",
+                 "Don’t know/Not sure",
+                 "Don’t know/Not Sure",
+                 "Don´t Know/Not Sure",
+                 "Don´t know/Not Sure",
+                 "Not asked or missing",
+                 "Not asked or Missing",
+                 "Don’t know/Refused/Missing",
+                 "Don’t know/Not Sure/Refused/Missing",
+                 "Don’t know/Not sure/Refused")
 
   data |>
     clean_names() |>
     # set unknown/missing values to NA
-    mutate(across(where(is.character), na_if, "Refused"),
-           across(where(is.character), na_if, "Don’t know/Not sure"),
-           across(where(is.character), na_if, "Don’t know/Not Sure"),
-           across(where(is.character), na_if, "Don´t know/Not Sure"),
-           across(where(is.character), na_if, "Not asked or missing"),
-           across(where(is.character), na_if, "Not asked or Missing"),
+    mutate(across(where(is.character), ~if_else(. %in% na_values, NA_character_, .))
     ) |>
+    # in kolomnaam staan bijv.  y year of x childeren, hiervan alleen het getal
     mutate(
-      across(any_of(c("age_when_told_had_diabetes")), str_remove, " years" )
+      across(any_of(c("age_when_told_had_diabetes")), str_extract, "[:digit:]" ),
+      across(any_of(c("number_of_children_in_household")), str_extract, "[:digit:]" )
     ) |>
     # convert columns to numeric
     mutate(across(any_of(numeric_vars), as.numeric)) |>
